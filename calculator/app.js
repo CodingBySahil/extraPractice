14c91ae7e85ac8a3c3905c0684309c6672bed449
CodingBySahil <muhammadsahil345737@gmail.com>
2024-01-17 20:47:43 +0500

    Calculator Just UI and some JS but not completed yet

---
+++ calculator/app.js
@@ -0,0 +1,89 @@
+let zero = document.querySelector("#zero").innerHTML,
+  one = document.querySelector("#one").innerHTML,
+  two = document.querySelector("#two").innerHTML,
+  three = document.querySelector("#three").innerHTML,
+  four = document.querySelector("#four").innerHTML,
+  five = document.querySelector("#five").innerHTML,
+  six = document.querySelector("#six").innerHTML,
+  seven = document.querySelector("#seven").innerHTML,
+  eight = document.querySelector("#eight").innerHTML,
+  nine = document.querySelector("#nine").innerHTML;
+let result=[];
+
+    // Code for zero funtion
+  function zeroFun(){
+    result.push(document.querySelector("#zero").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
+
+    // Code for one funtion
+  function oneFun(){
+    result.push(document.querySelector("#one").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
+
+    // Code for two funtion
+  function twoFun(){
+    result.push(document.querySelector("#two").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
+
+    // Code for three funtion
+  function threeFun(){
+    result.push(document.querySelector("#three").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
+
+    // Code for four funtion
+  function fourFun(){
+    result.push(document.querySelector("#four").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
+
+    // Code for five funtion
+  function fiveFun(){
+    result.push(document.querySelector("#five").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
+
+    // Code for six funtion
+  function sixFun(){
+    result.push(document.querySelector("#six").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
+
+    // Code for seven funtion
+  function sevenFun(){
+    result.push(document.querySelector("#seven").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
+
+    // Code for eight funtion
+  function eightFun(){
+    result.push(document.querySelector("#eight").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
+
+    // Code for nine funtion
+  function nineFun(){
+    result.push(document.querySelector("#nine").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
+
+    // Code for plus funtion
+  function plusFun(){
+    result.push(document.querySelector("#plus").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
+
+    // Code for minus funtion
+  function minusFun(){
+    result.push(document.querySelector("#minus").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
+
+    // Code for multiply funtion
+  function multiplyFun(){
+    result.push(document.querySelector("#multiply").innerHTML)
+    document.querySelector("#result").innerHTML += result.pop()
+  }
