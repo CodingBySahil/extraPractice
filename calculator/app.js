45709bd7aa612fd99a0f2eca7d257217781fa286
CodingBySahil <muhammadsahil345737@gmail.com>
2024-04-14 11:11:28 +0500

    calculator code modefied

--- calculator/app.js
+++ calculator/app.js
@@ -1,199 +1,28 @@
-let zero = document.querySelector("#zero").innerHTML,
-  one = document.querySelector("#one").innerHTML,
-  two = document.querySelector("#two").innerHTML,
-  three = document.querySelector("#three").innerHTML,
-  four = document.querySelector("#four").innerHTML,
-  five = document.querySelector("#five").innerHTML,
-  six = document.querySelector("#six").innerHTML,
-  seven = document.querySelector("#seven").innerHTML,
-  eight = document.querySelector("#eight").innerHTML,
-  nine = document.querySelector("#nine").innerHTML;
-let result = [],
-  oprators = [],
-  operands = [];
-// // code for opration
-// function equalCalculationFun(){
-//   for (let i = 0; i<result.length;i++){
-//     if(result[i] == "+"){
-//       oprators.push("+")
-//     }else if(result[i] == "-"){
-//       oprators.push("-")
-//     }else if(result[i] == "*"){
-//       oprators.push("*")
-//     }else{
-//       operands.push(result[i])
-//     }
-//   }
-//   let answer=[...operands];
-//   for (let i = 0; i < operands.length; i++) {
-//     Number(answer[i])
-//     switch (oprators[i]) {
-//       case "*":
-//         answer[0] = answer[0] * answer[1];
-//         answer.splice(1, 1);
-//         break;
-//       case "/":
-//         answer[0] = answer[0] / answer[1];
-//         answer.splice(1, 1);
-//         break;
-//       case "+":
-//         // Number(answer[0])
-//         // Number(answer[1])
-//         answer[0] = answer[0] + answer[1];
-//         answer.splice(1, 1);
-//         break;
-//       case "-":
-//         answer[0] = answer[0] - answer[1];
-//         answer.splice(1, 1);
-//         break;
-//     }
-//   }
-// }
+let displayValue = '0';
 
+function updateDisplay() {
+    document.getElementById('display').innerText = displayValue;
+}
 
-
-
-
-
-// code for opration
-let lastAnswer;
-function equalCalculationFun() {
-  for (let i = 0; i < result.length; i++) {
-    i=1;
-    if (result[i] == "+") {
-      let firstValue = Number(result[i-1])
-      let lastValue = Number(result[i+1])
-      let sum = firstValue + lastValue;
-      lastAnswer = sum;
-      result.splice(0,3,sum)
+function appendToDisplay(value) {
+    if (displayValue === '0' && value !== '.') {
+        displayValue = value;
+    } else {
+        displayValue += value;
     }
-    if (result[i] == "-") {
-      let firstValue = Number(result[i-1])
-      let lastValue = Number(result[i+1])
-      let minus = firstValue - lastValue;
-      lastAnswer = minus;
-      result.splice(0,3,minus)
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '0';
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue).toString();
+    } catch (error) {
+        displayValue = 'Error';
     }
-    if (result[i] == "*") {
-      let firstValue = Number(result[i-1])
-      let lastValue = Number(result[i+1])
-      let multiple = firstValue * lastValue;
-      lastAnswer = multiple;
-      result.splice(0,3,multiple)
-    }
-  }
-  document.querySelector("#result").innerHTML = lastAnswer
-}
-
-
-
-
-
-// Code for zero funtion
-function zeroFun() {
-  result.push(document.querySelector("#zero").innerHTML);
-  document.querySelector("#result").innerHTML += 0;
-  // result.push(0)
-}
-
-// Code for one funtion
-function oneFun() {
-  result.push(document.querySelector("#one").innerHTML);
-  document.querySelector("#result").innerHTML += 1;
-  // result.push(1);
-}
-
-// Code for two funtion
-function twoFun() {
-  result.push(document.querySelector("#two").innerHTML);
-  document.querySelector("#result").innerHTML += 2;
-  // result.push(2);
-}
-
-// Code for three funtion
-function threeFun() {
-  result.push(document.querySelector("#three").innerHTML);
-  document.querySelector("#result").innerHTML += 3;
-  // result.push(3);
-}
-
-// Code for four funtion
-function fourFun() {
-  result.push(document.querySelector("#four").innerHTML);
-  document.querySelector("#result").innerHTML += 4;
-  // result.push(4);
-}
-
-// Code for five funtion
-function fiveFun() {
-  result.push(document.querySelector("#five").innerHTML);
-  document.querySelector("#result").innerHTML += 5;
-  // result.push(5);
-}
-
-// Code for six funtion
-function sixFun() {
-  result.push(document.querySelector("#six").innerHTML);
-  document.querySelector("#result").innerHTML += 6;
-  // result.push(6);
-}
-
-// Code for seven funtion
-function sevenFun() {
-  result.push(document.querySelector("#seven").innerHTML);
-  document.querySelector("#result").innerHTML += 7;
-  // result.push(7)
-}
-
-// Code for eight funtion
-function eightFun() {
-  result.push(document.querySelector("#eight").innerHTML);
-  document.querySelector("#result").innerHTML += 8;
-  // result.push(8);
-}
-
-// Code for nine funtion
-function nineFun() {
-  result.push(document.querySelector("#nine").innerHTML);
-  document.querySelector("#result").innerHTML += 9;
-  // result.push(9);
-}
-
-// Code for plus funtion
-function plusFun() {
-  result.push(document.querySelector("#plus").innerHTML);
-  document.querySelector("#result").innerHTML += "+";
-  // result.push("+");
-}
-
-// Code for minus funtion
-function minusFun() {
-  result.push(document.querySelector("#minus").innerHTML);
-  document.querySelector("#result").innerHTML += "-";
-  // result.push("-");
-}
-
-// Code for multiply funtion
-function multiplyFun() {
-  result.push(document.querySelector("#multiply").innerHTML);
-  document.querySelector("#result").innerHTML += "*";
-  // result.push("*")
-}
-
-// Code for clear funtion
-function clearFun() {
-  result=[];
-  document.querySelector("#result").innerHTML = result
-}
-
-// Code for removing last value
-function removeLastValue() {
-  result.pop()
-  let result1= result;
-  result=[];
-  document.querySelector("#result").innerHTML = result
-  for(let i = 0;i<result1.length;i++){
-    document.querySelector("#result").innerHTML += result1[i]
-  }
-  result = result1
+    updateDisplay();
 }
